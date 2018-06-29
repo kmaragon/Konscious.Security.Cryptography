200598f19e8a9baa72316779e84a964e8087b84b
Keef Aragon <keef.aragon@konscious.net>
2018-06-29 00:55:14 -0700

    Initial checkin of Argon2id without tests

---
+++ Konscious.Security.Cryptography.Argon2/Argon2id.cs
@@ -0,0 +1,23 @@
+﻿namespace Konscious.Security.Cryptography
+{
+    /// <summary>
+    /// An implementation of Argon2 https://github.com/P-H-C/phc-winner-argon2
+    /// </summary>
+    public class Argon2id : Argon2
+    {
+        /// <summary>
+        /// Create an Argon2 for encrypting the given password using Argon2id
+        /// </summary>
+        /// <param name="password"></param>
+        public Argon2id(byte[] password)
+            : base(password)
+        {
+        }
+
+        internal override Argon2Core BuildCore(int bc)
+        {
+            return new Argon2idCore(bc);
+        }
+
+    }
+}
