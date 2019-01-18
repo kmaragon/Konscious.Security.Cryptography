67c32cfa7a56e25fd221a05183815dea0de6335d
Keef Aragon <keef.aragon@konscious.net>
2019-01-17 17:22:22 -0800

    Some updates on tests and pushing up a package bump to publish the PR from @alenas

--- Konscious.Security.Cryptography.Blake2.Test/HMACBlake2BSimdTests.cs
+++ Konscious.Security.Cryptography.Blake2.Test/HMACBlake2BSimdTests.cs
@@ -1,3 +1,6 @@
+using Blake2Core;
+using Xunit.Abstractions;
+
 namespace Konscious.Security.Cryptography.Test
 {
     using System;
@@ -9,55 +12,74 @@ namespace Konscious.Security.Cryptography.Test
     /// </summary>
     public class HMACBlake2BSimdTests
     {
+        private ITestOutputHelper _output;
+        
+        public HMACBlake2BSimdTests(ITestOutputHelper helper)
+        {
+            _output = helper;
+        }
+        
         [Fact]
         public void CorrectlyComputesShortHash512()
         {
-            AssertMatch(0x5069230, 9, 512, 512);
+            AssertMatch(_output, 0x5069230, 9, 512, 512);
         }
 
         [Fact]
+        public void CorrectlyComputesShortHash128()
+        {
+            AssertMatch(_output, 0x102193, 3, 128, 128);
+        }
+        
+        [Fact]
         public void CorrectlyComputesHash256With512BitKey()
         {
-            AssertMatch(0x80f39c2, 157, 256, 512);
+            AssertMatch(_output, 0x80f39c2, 157, 256, 512);
         }
 
         [Fact]
         public void CorrectlyComputesHash256With128BitKey()
         {
-            AssertMatch(0x12c7361f, 195, 256, 128);
+            AssertMatch(_output, 0x12c7361f, 195, 256, 128);
         }
 
         [Fact]
         public void CorrectlyComputesExactBoundaryHash512()
         {
-            AssertMatch(0x5fc00893, 64, 512, 512);
+            AssertMatch(_output, 0x5fc00893, 64, 512, 512);
         }
 
         [Fact]
         public void CorrectlyComputesLongNonBoundary512()
         {
-            AssertMatch(0x750a6700, 176, 512, 512);
+            AssertMatch(_output, 0x750a6700, 176, 512, 512);
         }
 
         [Fact]
         public void CorrectlyComputesLongBoundaryAligned512()
         {
-            AssertMatch(0x240f5a03, 640, 512, 512);
+            AssertMatch(_output, 0x240f5a03, 640, 512, 512);
         }
 
         [Fact]
         public void CorrectlyComputesLongBoundary512WithNonBoundaryKey()
         {
-            AssertMatch(0x3f078897, 640, 512, 232);
+            AssertMatch(_output, 0x3f078897, 640, 512, 232);
         }
 
         [Fact]
         public void CorrectlyComputesLongNonBoundary512WithNonBoundaryKey()
         {
-            AssertMatch(0xa089e023, 521, 512, 368);
+            AssertMatch(_output, 0xa089e023, 521, 512, 368);
         }
 
-        private static void AssertMatch(uint seed, int dataSize, int hashSize, int keySize)
+        [Fact]
+        public void CorrectlyComputesLargeAligned128()
+        {
+            AssertMatch(_output, 0x1947850, 1024*1024*10, 128, 128);
+        }
+        
+        private static void AssertMatch(ITestOutputHelper output, uint seed, int dataSize, int hashSize, int keySize)
         {
             var rand = new Random((int)seed);
             var data = new byte[dataSize];
@@ -66,13 +88,32 @@ namespace Konscious.Security.Cryptography.Test
             rand.NextBytes(data);
             rand.NextBytes(key);
 
+            var start = DateTime.Now;
             var slow = new HMACBlake2B(key, hashSize, () => new Blake2bSlow(hashSize / 8));
-            var normal = new HMACBlake2B(key, hashSize, () => new Blake2bSimd(hashSize / 8));
-
             slow.Initialize();
+            var slowHash = slow.ComputeHash(data);
+            var slowTime = (DateTime.Now - start).TotalMilliseconds * 1000;
+            
+            start = DateTime.Now;
+            var normal = new HMACBlake2B(key, hashSize, () => new Blake2bSimd(hashSize / 8));
             normal.Initialize();
+            var normalHash = normal.ComputeHash(data);
+            var normalTime = (DateTime.Now - start).TotalMilliseconds * 1000;
 
-            Assert.Equal(slow.ComputeHash(data), normal.ComputeHash(data));
+            start = DateTime.Now;
+            var b2core = Blake2Core.Blake2B.Create(new Blake2BConfig()
+            {
+                Key = key,
+                OutputSizeInBits = hashSize
+            });
+            b2core.Init();
+            b2core.Update(data);
+            var b2CoreHash = b2core.Finish();
+            var b2CoreTime = (DateTime.Now - start).TotalMilliseconds * 1000;
+            
+            output.WriteLine($"Slow path took {slowTime} micros, Normal path took {normalTime} micros, 3rd Party path took {b2CoreTime} micros");
+            Assert.Equal(slowHash, normalHash);
+            Assert.Equal(b2CoreHash, normalHash);
         }
     }
 }
